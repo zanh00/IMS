806b3a8b379dfb8228fc5bc82badc5e5b1aa2895
zan <zanhertis@gmail.com>
2023-07-03 15:06:12 +0200

    comments added and minor improvements

--- main.cpp
+++ main.cpp
@@ -4,21 +4,25 @@
 #include <vector>
 #include <random>
 
-///////////////// USER INCLUDES ////////////////////
+/*      USER INCLUDES     */
+
 #include "Item.h"
 #include "ExactItem.h" 
 
 using namespace std;
 
-/////////////////// DEFINES ////////////////////
+/*      DEFINES             */
+
 #define MAX_NUMBER_OF_ITEM 200
 
-////////////////// GLOBAL VARIABLES //////////////////
-map<string, Item> GeneralItem;
-map<uint32_t, ExactItem> SpeificItem; 
-map<string, vector<uint32_t>> GeneralItemIDs;
+/*       GLOBAL VARIABLES       */
 
-///////////////// FUNCTIONS //////////////////////
+map<string, Item> GeneralItem;                      // holds general items name and parameters (descp., quant.)
+map<uint32_t, ExactItem> SpeificItem;               // holds all specific item (exacItem obejcts) with their ID
+map<string, vector<uint32_t>> GeneralItemIDs;       // holds all specific item IDs for each general item
+
+/*      FUNCTIONS           */
+
 void addItemOne();
 uint32_t IDgenerator();
 
@@ -37,6 +41,9 @@ int main() {
 
 }
 
+/**
+    @brief Function adds new items to the datatbase
+*/ 
 void addItemOne()
 {
     string getName;
@@ -49,12 +56,12 @@ void addItemOne()
     cout << "Provide the name of an item you wish to add to the inventory" << endl;
     getline(cin, getName);
 
-    if(GeneralItem.count(getName) != 0)
+    if(GeneralItem.count(getName) != 0)                                                                 // if item already exsists than get its description
     {
         getDescription = GeneralItem[getName].getDescription();
         nameExsist = true;
     }
-    else
+    else                                                                                                // if not than ask user to provide it
     {
         cout << "Provide a description for the item: " << getName << endl;
         getline(cin, getDescription);
@@ -62,31 +69,35 @@ void addItemOne()
     }
 
     cout << "How many items would you like to add?" << endl;
-    while (!(cin >> numOfItemsToAdd) || numOfItemsToAdd < 0 || numOfItemsToAdd > MAX_NUMBER_OF_ITEM) {
+    while (!(cin >> numOfItemsToAdd) || numOfItemsToAdd < 0 || numOfItemsToAdd > MAX_NUMBER_OF_ITEM) {  // user enters a valid number between 0 and Max number of items that can be added at once
         
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
     
-    if(!nameExsist)
+    if(!nameExsist)                                                                                     // if gneral item doesnt exist than create it  and add it to the map
     {
         GeneralItem[getName] = Item(getName, getDescription, numOfItemsToAdd);
     }
-    else
+    else                                                                                                // if it does checks how many specific items it already has and sums it wiht num. to be added
     {
         numOfPreviousItems = GeneralItem[getName].getQuantity();
         GeneralItem[getName] = Item(getName, getDescription, numOfPreviousItems + numOfItemsToAdd);
     }
 
-    for (int i = 0; i < numOfItemsToAdd; i++)
+    cout << numOfItemsToAdd << " new items added with the following IDs:" << endl;
+    for (int i = 0; i < numOfItemsToAdd; i++)                                                           // adds new spcific items and its IDs to the vector
     {
         ID = IDgenerator();
         SpeificItem[ID] = ExactItem(getName, getDescription, ID);
         GeneralItemIDs[getName].push_back(ID);
+        cout << i + 1 << " " << ID << endl;
     } 
 
 }
 
+/// @brief generates a random ID and chechks the databes if it is free (repets if necessary)
+/// @return the unique ID
 uint32_t IDgenerator()
 {
     uint32_t ID;
@@ -100,3 +111,5 @@ uint32_t IDgenerator()
     
     return ID;
 }
+
+
